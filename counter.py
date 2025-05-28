0d79a3eb6debc886b534435ba13ac13aad8944df
Nikunj Taneja <niktaneja99@gmail.com>
2025-05-28 22:48:58 +0800

    add a basic state machine

---
+++ counter.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+class StateMachine(ABC):
+    @abstractmethod
+    def apply(self, command: str):
+        pass
+
+class Counter(StateMachine):
+    def __init__(self):
+        self.value: int = 0
+
+    def apply(self, command: str):
+        if command == "INC":
+            self.value += 1
+        elif command == "RESET":
+            self.value = 0
+        else:
+            raise ValueError(f"Invalid command: {command}")
+
+    def get_value(self):
+        return self.value
